8ff0c6407c1729f3590ca130fac536fe27a523ed
Pheng Heong TAN <phtan90@gmail.com>
2016-05-07 15:44:57 +0800

    Created static page. Updated views.py of app

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -5,8 +5,8 @@ def index(request):
 
     context_dict = {'boldmessage': "I am bold font from the context"}
 
-    # return HttpResponse("Rango says hello world! <br /> <a href='/rango/about'>About</a>")
     return render(request, 'rango/index.html', context_dict)
 
 def about(request):
-    return HttpResponse("Rango says here is the about page.<br /><a href='/rango'>Home</a>")
+    context_dict = {}
+    return render(request, 'rango/about.html', context_dict)
