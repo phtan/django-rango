9da4b56764332eac9d242e41401382eb79067867
Pheng Heong TAN <phtan90@gmail.com>
2016-05-17 15:45:20 +0800

    Created list of links to categories. Re. Chapter 7.3.8

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -1,9 +1,18 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
+    slug = models.SlugField()
+
+    def save(self, *args, **kwargs):
+        # if self.id is None:
+        # self.slug = slugify(self.name)
+
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
 
     def __unicode__(self):
         return self.name
--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,12 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category, Page
 
 def index(request):
 
-    context_dict = {'boldmessage': "I am bold font from the context"}
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'boldmessage': "I am bold font from the context", 'categories': category_list}
+
 
     return render(request, 'rango/index.html', context_dict)
 
 def about(request):
     context_dict = {}
     return render(request, 'rango/about.html', context_dict)
+
+def category(request, category_name_slug):
+
+    context_dict = {}
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        context_dict['category_name'] = category.name
+
+        pages = Page.objects.filter(category=category)
+
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        pass # Don't do anything. The template displays the 'No category' message for us.
+
+    return render(request, 'rango/category.html', context_dict)
