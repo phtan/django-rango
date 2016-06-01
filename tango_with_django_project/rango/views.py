f715c946d0020cdb5c7916ce7f1594d5f300f3b1
Pheng Heong TAN <phtan90@gmail.com>
2016-06-01 17:09:16 +0800

    Created form to add page for a given category. Re. chapter 8.3.1

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category, Page
 from rango.forms import CategoryForm
+from rango.forms import PageForm
 
 def index(request):
 
@@ -23,6 +24,7 @@ def category(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
         context_dict['category_name'] = category.name
+        context_dict['category_name_slug'] = category.slug
 
         pages = Page.objects.filter(category=category)
 
@@ -48,3 +50,27 @@ def add_category(request):
         form = CategoryForm()
 
     return render(request, 'rango/add_category.html', {'form': form})
+
+def add_page(request, category_name_slug):
+
+    try:
+        cat = Category.objects.get(slug=category_name_slug)
+    except CategoryDoesNotExist:
+        cat = None
+
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+        if form.is_valid():
+            if cat:
+                page = form.save(commit=False)
+                page.category = cat
+                page.views = 0
+                page.save()
+                return category(request, category_name_slug)
+        else:
+            print form.errors
+    else:
+        form = PageForm()
+
+    context_dict = {'form': form, 'category': cat, 'category_name_slug': category_name_slug}
+    return render(request, 'rango/add_page.html', context_dict)
