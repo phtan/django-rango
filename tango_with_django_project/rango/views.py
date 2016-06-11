6aa65f20e40413e91b311ecda1b7d52ea8c678b9
Pheng Heong TAN <phtan90@gmail.com>
2016-06-11 19:04:47 +0800

    Created restrictions on add-a-page and add-a-category. Re. chapter 9.8

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -36,6 +36,7 @@ def category(request, category_name_slug):
 
     return render(request, 'rango/category.html', context_dict)
 
+@login_required
 def add_category(request):
     if request.method == 'POST':
         form  = CategoryForm(request.POST)
@@ -52,6 +53,7 @@ def add_category(request):
 
     return render(request, 'rango/add_category.html', {'form': form})
 
+@login_required
 def add_page(request, category_name_slug):
 
     try:
