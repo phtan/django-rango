b8e33fe5bb8aab8bff79624bd204ea67d6907e12
Pheng Heong TAN <phtan90@gmail.com>
2016-06-11 18:24:06 +0800

    Created login page. Re. chapter 9.5.5

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from rango.models import Category, Page
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
+from django.contrib.auth import authenticate, login
 
 def index(request):
 
@@ -106,3 +107,22 @@ def register(request):
     return render(request,
           'rango/register.html',
           {'user_form': user_form, 'profile_form': profile_form, 'registered': registered} )
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect('/rango/')
+            else:
+                return HttpResponse('Your Rango account is disabled.')
+        else:
+            print "Invalid login details: {0}, {1}".format(username, password)
+            return HttpResponse('Invalid login details were supplied.')
+    else:
+        return render(request, 'rango/login.html', {})
