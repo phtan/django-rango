6e8a76a68307666ee05dc6d4d34c84c1334bbda2
Pheng Heong TAN <phtan90@gmail.com>
2016-06-11 18:42:59 +0800

    Created logout link, and restricted an area. Re. chapter 9.7

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from rango.models import Category, Page
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 def index(request):
 
@@ -126,3 +127,12 @@ def user_login(request):
             return HttpResponse('Invalid login details were supplied.')
     else:
         return render(request, 'rango/login.html', {})
+
+@login_required
+def restricted(request):
+    return HttpResponse("Since you're logged in, you can see this text!")
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect('/rango/')
--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -98,3 +98,6 @@ TEMPLATE_DIRS = (
 # Media (eg. uploads)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # Absolute path to the media directory
+
+# Restricting access
+LOGIN_URL = '/rango/login/'
